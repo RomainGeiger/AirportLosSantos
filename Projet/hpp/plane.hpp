0dd8741d00601414d26cd603840c56cd70666e73
Romain <romain.geiger@student.junia.com>
2025-11-19 15:14:13 +0100

    ajout altitude

--- Projet/hpp/plane.hpp
+++ Projet/hpp/plane.hpp
@@ -5,19 +5,22 @@ class Plane {
 	private:
 		float posx_;
 		float posy_;
+		float posz_; //Altitude
 		//int id;
 		Etat state_;
 
 	public:
-		Plane(float pos_x, float pos_y, Etat state) {
+		Plane(float pos_x, float pos_y, float pos_z, Etat state) {
 			posx_ = pos_x;
 			posy_ = pos_y;
+			posz_ = pos_z;
 			state_ = state;
 		}
 
 		//Getter
 		float get_x() { return posx_; }
 		float get_y() { return posy_; }
+		float get_z() { return posz_; }
 		Etat get_state() { return state_; }
 
 		//Setter
