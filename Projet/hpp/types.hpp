433f71e323a5f865598b129925757a2b4e1a5063
felis4ka <felix.beuvry25@gmail.com>
2025-11-25 14:30:29 +0100

    types.cpp update

---
+++ Projet/hpp/types.hpp
@@ -0,0 +1,53 @@
+#pragma once 
+#include <cmath>
+
+namespace ls {
+
+	struct Vec2 {
+		float x = 0.f;
+		float y = 0.f;
+
+		Vec2() = default;
+		Vec2(float x_, float y_) : x(x_), y(y_) {}
+
+		//opérations basiques
+
+		Vec2 operator+(const Vec2& o) const {
+			return Vec2(x + o.x, y + o.y);
+		}
+
+		Vec2 operator-(const Vec2& o) const {
+			return Vec2(x - o.x, y - o.y);
+		}
+
+		Vec2 operator*(float s) const {
+			return Vec2(x * s, y * s);
+		}
+
+		//opérations composées
+
+		Vec2 operator+=(const Vec2& o) {
+			x += o.x;
+			y += o.y;
+			return *this;
+		}
+		Vec2 operator-=(const Vec2& o) {
+			x -= o.x;
+			y -= o.y;
+			return *this;
+		}
+		Vec2 operator*=(float s) {
+			x *= s;
+			y *= s;
+			return *this;
+		}
+		
+		float length() const {
+			return ::std::sqrt(x * x + y * y);
+		}
+	};
+
+	inline Vec2 operator*(float s, const Vec2& v) {
+		return Vec2(v.x * s, v.y * s);
+	}
+}
